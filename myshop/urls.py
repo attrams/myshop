a1468ded6ba5782935edb5566bbe028fd36d0e80
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-23 21:38:35 +0000

    add media url

--- myshop/urls.py
+++ myshop/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path(route='admin/', view=admin.site.urls),
     path(route='', view=include('shop.urls', namespace='shop')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
