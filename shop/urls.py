0e9010b0b25795ab7c57892c59b035076637257a
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-23 20:45:06 +0000

    add product list and detail views

---
+++ shop/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path(route='', view=views.product_list, name='product_list'),
+    path(
+        route='<slug:category_slug>/',
+        view=views.product_list,
+        name='product_list_by_category'
+    ),
+    path(
+        route='<int:id>/<slug:slug>/',
+        view=views.product_detail,
+        name='product_detail'
+    ),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product, Category
 
 # Create your views here.
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    if category_slug:
+        category = get_object_or_404(klass=Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(
+        request=request,
+        template_name='shop/product/list.html',
+        context={
+            'category': category,
+            'categories': categories,
+            'products': products,
+        }
+    )
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(
+        klass=Product,
+        id=id,
+        slug=slug,
+        available=True
+    )
+
+    return render(request=request, template_name='shop/product/detail.html', context={'product': product})
