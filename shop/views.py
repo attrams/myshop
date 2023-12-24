bd0cf8ea2b34dde543e56112b873ef0a991a1665
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-24 13:39:02 +0000

    add CartAddProduct form

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product, Category
-
+from cart.forms import CartAddProductForm
 # Create your views here.
 
 
@@ -32,5 +32,6 @@ def product_detail(request, id, slug):
         slug=slug,
         available=True
     )
+    cart_product_form = CartAddProductForm()
 
-    return render(request=request, template_name='shop/product/detail.html', context={'product': product})
+    return render(request=request, template_name='shop/product/detail.html', context={'product': product, 'cart_product_form': cart_product_form})
