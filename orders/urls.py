a4deb924a0a08b12fa59465a0ed263fc44455bca
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-25 03:20:20 +0000

    edit app_name

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'order'
+app_name = 'orders'
 
 urlpatterns = [
     path(route='create/', view=views.order_create, name='order_create'),
