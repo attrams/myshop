f250deed286eb29371501a3d74b6d6fd1af46566
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-26 22:46:45 +0000

    generate pdf invoice

--- orders/admin.py
+++ orders/admin.py
@@ -68,6 +68,15 @@ def order_detail(obj):
     return mark_safe(f'<a href="{url}">View</a>')
 
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+
+    return mark_safe(f'<a href="{url}" target="_blank">PDF</a>')
+
+
+order_pdf.short_description = 'Invoice'
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -78,7 +87,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'first_name', 'last_name', 'email',
         'address', 'postal_code', 'city', 'paid',
-        order_payment, 'created', 'updated', order_detail
+        order_payment, 'created', 'updated', order_detail,
+        order_pdf
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
