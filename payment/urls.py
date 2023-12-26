12167a37a48d5904e9bd61f6d5770322c778d1ba
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-26 15:19:50 +0000

    add stripe_webhook

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from . import webhooks
 
 app_name = 'payment'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path(route='process/', view=views.payment_process, name='process'),
     path(route='completed/', view=views.payment_completed, name='completed'),
     path(route='canceled/', view=views.payment_canceled, name='canceled'),
+    path(route='webhook/', view=webhooks.stripe_webhook, name='stripe-webhook'),
 ]
