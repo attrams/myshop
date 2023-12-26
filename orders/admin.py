8b81f479e7cbf0bf369974e71bd206a2058972bb
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-26 19:52:05 +0000

    add export_to_csv action

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,51 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+import datetime
+import csv
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
 
 # Register your models here.
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+
+    writer = csv.writer(response)
+
+    print(opts.get_fields())
+
+    fields = [
+        field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many
+    ]
+
+    print(fields)
+
+    # write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+
+    # write data rows
+    for obj in queryset:
+        data_row = []
+
+        for field in fields:
+            value = getattr(obj, field.name)
+
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+
+            data_row.append(value)
+
+        writer.writerow(data_row)
+
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 def order_payment(obj):
@@ -34,3 +76,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
