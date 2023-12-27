398b28ff8bffe7c72711d398037e6dd496c1e4a0
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-27 16:18:26 +0000

    apply coupon to shopping cart

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
 
 # Create your views here.
 
@@ -41,5 +42,6 @@ def cart_detail(request):
             'quantity': item['quantity'],
             'override': True
         })
+    coupon_apply_form = CouponApplyForm()
 
-    return render(request=request, template_name='cart/detail.html', context={'cart': cart})
+    return render(request=request, template_name='cart/detail.html', context={'cart': cart, 'coupon_apply_form': coupon_apply_form})
